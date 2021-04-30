6d5b1d12b347dd5b1382c725a3540cac9868a755
thevoyagertwo <thevoyagertwo@protonmail.com>
2021-04-30 13:13:04 +0100

    converted strings to char

--- src/main/java/Enigma/Rotors.java
+++ src/main/java/Enigma/Rotors.java
@@ -5,21 +5,21 @@ import java.io.Serializable;
 public class Rotors {
 
     private String name;     // name of the rotor cypher: https://en.wikipedia.org/wiki/Enigma_rotor_details
-    private String[] cypher; // contains the scrambled alphabet
+    private char[] cypher; // contains the scrambled alphabet
 
     public Rotors(String name) {
         this.name = name;
         switch (name) {
             case "I":
-                this.cypher = new String[]{"E", "K", "M", "F", "L", "G", "D", "Q", "V", "Z", "N", "T", "O", "W", "Y", "H", "X", "U", "S", "P", "A", "I", "B", "R", "C", "J"};
+                this.cypher = new char[]{'E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J'};
                 break;
 
             case "II":
-                this.cypher = new String[]{"A", "J", "D", "K", "S", "I", "R", "U", "X", "B", "L", "H", "W", "T", "M", "C", "Q", "G", "Z", "N", "P", "Y", "F", "V", "O", "E"};
+                this.cypher = new char[]{'A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E'};
                 break;
 
             default:
-                this.cypher = new String[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+                this.cypher = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
                 break;
         }
     }
@@ -35,11 +35,11 @@ public class Rotors {
         this.name = name;
     }
 
-    public String[] getCypher() {
+    public char[] getCypher() {
         return this.cypher;
     }
 
-    public void setCypher(String[] cypher) {
+    public void setCypher(char[] cypher) {
         this.cypher = cypher;
     }
 
